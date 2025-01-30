aa63fa034bacd8a7cde8b62abf0526d61a43bda7
RodolpheACHY <rod23_reseaux@yahoo.fr>
2025-01-30 05:34:50 +0100

    del suppression ou modif des commentaires dans /pages/index.js du main

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -22,7 +22,6 @@ import {
  } from "../mainSearchInput.js";
 import { displayRecipes } from "../cards.js";
 import { displayTags } from "../tagSelected.js";
-import { displayFilteredRecipes } from "../mainSearchTag.js";
 import { sanitizeInput  } from "../utils.js";
 
 
@@ -31,8 +30,8 @@ import { sanitizeInput  } from "../utils.js";
  * 
  * Cette fonction configure :
  * - Les événements de clic et d'entrée pour les menus déroulants.
- * - La gestion de la recherche (barre de recherche principale et menus).
- * - L'affichage des recettes et des tags.
+ * - La gestion de la recherche (barre de recherche principale et menus déroulants).
+ * - L'affichage initial des recettes et des tags.
  */
 function init() {
   // Gère l'affichage des listes au clic sur les boutons dropdown
@@ -47,50 +46,45 @@ function init() {
   const buttons = document.querySelectorAll(".main__dropbtn");
   buttons.forEach((button) => {button.addEventListener("click", toggleRotate);});
   
-  // gère le filtre des ingrédients affichés dans le dropdown
+  // Gère le filtre des ingrédients affichés dans le dropdown
   const inputSearchIngredient = document.getElementById("inputSearchIngredient");
   inputSearchIngredient.addEventListener("input", handleSearchListIngredients);
 
-  // gère le filtre des appareils affichés dans le dropdown
+  // Gère le filtre des appareils affichés dans le dropdown
   const inputSearchDevice = document.getElementById("inputSearchDevice");
   inputSearchDevice.addEventListener("input", handleSearchListAppliances);
 
-  // gère le filtre des ustensiles affichés dans le dropdown
+  // Gère le filtre des ustensiles affichés dans le dropdown
   const inputSearchUstensile = document.getElementById("inputSearchUstensile");
   inputSearchUstensile.addEventListener("input", handleSearchListUstensiles);
 
   // Récupère les datas des recettes
   const recipes = getRecipes();
 
-  //empèche les datas des recettes
+  // Empèche les datas des recettes
   const form = document.getElementById("header__search-container__form");
   form.addEventListener('submit', handleSubmit);
-
 
   // Recherche via la barre principale
   const searchButton = document.getElementById("header__search-container__button");
   const searchInput = document.getElementById("header__search-container__input");
   
-  // Recherche déclenchée à chaque saisie dans la barre principale
-  // searchInput.addEventListener("input", () => handleSearch(recipes, searchInput.value));
+  // Recherche déclenchée à chaque saisie dans la barre de recherche principale
   searchInput.addEventListener("input", () => {
     const sanatyzedInputValue = sanitizeInput(searchInput.value);
     handleSearch(recipes, sanatyzedInputValue);
   });
 
   // Recherche déclenchée au clic sur le bouton de recherche
-  // searchButton.addEventListener("click", () => handleSearch(recipes, searchInput.value));
   searchButton.addEventListener("click", () => {
     const sanatyzedInputValue = sanitizeInput(searchInput.value);
     handleSearch(recipes, sanatyzedInputValue);
   });
   
-
-
-  // affiche les recettes initiales
+  // Affichage initial des recettes
   displayRecipes(recipes);
 
-  // affichage initial des listes des dropdowns
+  // Affichage initial des listes des dropdowns
   initDropdownIngredient(recipes);
   initDropdownAppliances(recipes);
   initDropdownUstensiles(recipes);
@@ -98,20 +92,15 @@ function init() {
   //affichage des tags sélectionnés
   displayTags();
 
-  // handleSearchListIngredients();
-
   // clear inputs
   clearInput("header__search-container__input", "input-xmark-icon", recipes);
   clearInput("inputSearchIngredient", "iconXmarkInputIngredients", recipes);
   clearInput("inputSearchDevice", "iconXmarkInputDevices", recipes);
   clearInput("inputSearchUstensile", "iconXmarkInputUstensiles", recipes);
 
-  const logo = document.getElementById("header__logo");
-  //logo.addEventListener("click", displayFilteredRecipes);
-  logo.addEventListener("click", handleSearch);
-
+  // Recherche déclenchée lorsqu'un événement personnalisé "filtersUpdated" est émis
   document.addEventListener("filtersUpdated", () => handleSearch(recipes, searchInput.value));
 }
 
-//init();
+// Initialise l'application une fois le DOM chargé
 document.addEventListener("DOMContentLoaded", init);
